5e8ad5246b00239dd4c7d2bbc03d0caaacb93913
CravingTheories <carsonwt07@gmail.com>
2025-09-04 17:07:57 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@ Carson Taylor
 9/4/2025
 */
 
+// FOR CONVENIENCE, UTILIZE THIS ONLINE COMPILER: https://www.onlinegdb.com/online_c++_compiler
+
+
 #include <iostream>
 #include <cmath> // Used for pow function
 
@@ -11,12 +14,19 @@ using namespace std;
 int main() { 
 
     double bmi; // BMI: Body Mass Index.
-    int weight = 130; // The person's weight
-    int height = 61; // The person's height
+    int weight; // The person's weight
+    int height; // The person's height
 
-    bmi = (weight / pow(height, 2)) * 703;
+    cout << "Enter your weight: ";
+    cin >> weight; // Get the user input for weight in pounds
 
-    cout << "The BMI for this person is : " << bmi; // Display the BMI to the console
+    cout << "Enter your height: ";
+    cin >> height; // Get the user input for height in inches
+
+    bmi = (weight / pow(height, 2)) * 703; // Calculate BMI without having to use kilograms or meters
+    bmi = floor((bmi * 100) + .5) / 100; // Rounds BMI to two decimal places -- mainly to look nice
+
+    cout << "Body Mass Index (BMI): " << bmi; // Display the BMI to the console
     
     return 0;
 
